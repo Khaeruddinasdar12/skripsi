a0d56dce586af8880956a9841f397c7941e2dd65
Khaeruddinasdar12 <46445417+Khaeruddinasdar12@users.noreply.github.com>
2020-07-15 00:37:48 +0800

    SawahController

--- app/Http/Controllers/Api/SawahController.php
+++ app/Http/Controllers/Api/SawahController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
 class SawahController extends Controller
 {
     
--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -89,6 +89,6 @@ class UserController extends Controller
 
                     }
 
-                    return response()->json(compact('user'));
+                    return response()->json(compact('user', 'db'));
             }
 }
