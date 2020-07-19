9d583951291b49f88c52f68b54d11d07c823ee22
Khaeruddinasdar12 <46445417+Khaeruddinasdar12@users.noreply.github.com>
2020-07-19 22:59:17 +0800

    UserController untuk web admin

--- routes/web.php
+++ routes/web.php
@@ -61,6 +61,7 @@ Route::prefix('admin')->namespace('Admin')->group(function () {
 
 	// RUTE MENU MANAGE USER
 	Route::get('manage-user', 'UserController@index')->name('index.manage-user'); //menampilkan hal. data user
+	Route::get('manage-user/{id}', 'UserController@verified');
 	// END RUTE MANAGE USER
 
 	Route::namespace('Auth')->group(function () {
