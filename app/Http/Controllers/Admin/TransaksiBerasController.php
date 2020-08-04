880ce280aaa174a1d6b4fcf992b6c6977e6beeb8
adheee <adheejr77@gmail.com>
2020-08-04 12:14:55 +0800

    add file transaksiberas

--- app/Http/Controllers/Admin/TransaksiBerasController.php
+++ app/Http/Controllers/Admin/TransaksiBerasController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Auth;
 use App\TransaksiBeras;
+
 class TransaksiBerasController extends Controller
 {
     public function __construct()
@@ -15,22 +16,20 @@ class TransaksiBerasController extends Controller
 
     public function index() //menampilkan hal. data transaksi beras
     {
-    	//mengurutkan dari terbaru ke terlama (descending)
-    	$data = TransaksiBeras::where('status', '0')
-    			->with('users:id,name')
-    			->orderBy('created_at', 'desc')
-    			->paginate(10);
-    			
-    	return $data;
+        //mengurutkan dari terbaru ke terlama (descending)
+        $data = TransaksiBeras::where('status', '0')
+            ->with('users:id,name')
+            ->orderBy('created_at', 'desc')
+            ->paginate(10);
+
+        return $data;
     }
 
     public function riwayat() //menampilkan hal. data riwayat transaksi beras
     {
-    	
     }
 
     public function status($id) // mengubah status pembelian beras menjadi riwayat
     {
-    	
     }
 }
