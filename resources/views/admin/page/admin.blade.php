e796a125ff15355b64ed86ddbef66da57455c673
adheee <adheejr77@gmail.com>
2020-08-21 11:07:52 +0800

    add search form

--- resources/views/admin/page/admin.blade.php
+++ resources/views/admin/page/admin.blade.php
@@ -64,30 +64,32 @@
               </div>
               <div class="kt-portlet__head-toolbar">
                 <div class="kt-portlet__head-actions">
-                  <div class="kt-header__topbar-item kt-header__topbar-item--search dropdown show" id="kt_quick_search_toggle">
-                    <div class="kt-header__topbar-wrapper" data-toggle="dropdown" data-offset="10px,0px" aria-expanded="true">
-                      <span class="kt-header__topbar-icon"><i class="flaticon2-search-1"></i></span>
+                  <div class="dropdown show" id="kt_quick_search_toggle">
+                    <div class="search-form" data-toggle="dropdown" data-offset="10px,0px" aria-expanded="true">
+                      <i class="fa fa-search"></i>
+                      <span class="border-right"></span>
                     </div>
-                    <div class="dropdown-menu dropdown-menu-fit dropdown-menu-right dropdown-menu-anim dropdown-menu-lg show" x-placement="bottom-end" style="position: absolute; will-change: transform; top: 0px; left: 0px; transform: translate3d(-268px, 80px, 0px);">
-                      <div class="kt-quick-search kt-quick-search--dropdown kt-quick-search--result-compact" id="kt_quick_search_dropdown">
-                        <form method="get" class="kt-quick-search__form">
-                          <div class="input-group">
-                            <div class="input-group-prepend"><span class="input-group-text"><i class="flaticon2-search-1"></i></span></div>
-                            <input type="text" class="form-control kt-quick-search__input" placeholder="Search...">
-                            <div class="input-group-append"><span class="input-group-text"><i class="la la-close kt-quick-search__close"></i></span></div>
-                          </div>
+                    <div class="dropdown-menu dropdown-menu-fit dropdown-menu-right dropdown-menu-anim dropdown-menu-lg show search-form-dropdown" x-placement="bottom-end" style="position: absolute; will-change: transform; top: 0px; left: 1000px; transform: translate3d(-268px, 80px, 0px);">
+                      <div class="kt-input-icon kt-input-icon--right">
+                        <form action="" method="get">
+                          <input type="text" class="form-control" placeholder="Cari" id="generalSearch">
+                          <span class="kt-input-icon__icon kt-input-icon__icon--right">
+                            <button type="button">
+                              <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1" class="kt-svg-icon">
+                                <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                  <rect x="0" y="0" width="24" height="24"></rect>
+                                  <path d="M14.2928932,16.7071068 C13.9023689,16.3165825 13.9023689,15.6834175 14.2928932,15.2928932 C14.6834175,14.9023689 15.3165825,14.9023689 15.7071068,15.2928932 L19.7071068,19.2928932 C20.0976311,19.6834175 20.0976311,20.3165825 19.7071068,20.7071068 C19.3165825,21.0976311 18.6834175,21.0976311 18.2928932,20.7071068 L14.2928932,16.7071068 Z" fill="#000000" fill-rule="nonzero" opacity="0.3"></path>
+                                  <path d="M11,16 C13.7614237,16 16,13.7614237 16,11 C16,8.23857625 13.7614237,6 11,6 C8.23857625,6 6,8.23857625 6,11 C6,13.7614237 8.23857625,16 11,16 Z M11,18 C7.13400675,18 4,14.8659932 4,11 C4,7.13400675 7.13400675,4 11,4 C14.8659932,4 18,7.13400675 18,11 C18,14.8659932 14.8659932,18 11,18 Z" fill="#000000" fill-rule="nonzero"></path>
+                                </g>
+                              </svg>
+                            </button>
+                          </span>
                         </form>
-                        <div class="kt-quick-search__wrapper kt-scroll ps" data-scroll="true" data-height="325" data-mobile-height="200" style="height: 325px; overflow: hidden;">
-                          <div class="ps__rail-x" style="left: 0px; bottom: 0px;">
-                            <div class="ps__thumb-x" tabindex="0" style="left: 0px; width: 0px;"></div>
-                          </div>
-                          <div class="ps__rail-y" style="top: 0px; right: 0px;">
-                            <div class="ps__thumb-y" tabindex="0" style="top: 0px; height: 0px;"></div>
-                          </div>
-                        </div>
                       </div>
                     </div>
                   </div>
+                </div>
+                <div class="kt-portlet__head-actions">
                   <a href="#" class="btn btn-clean btn-icon btn-icon-md btn-tambah-data" data-toggle="modal" data-target="#modal-tambah-admin">
                     <i class="flaticon2-add"></i>
                   </a>
