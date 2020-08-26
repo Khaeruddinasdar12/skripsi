2b5218ab0b014b9c6696283fd15db1e0d54622a5
Khaeruddinasdar12 <46445417+Khaeruddinasdar12@users.noreply.github.com>
2020-08-26 08:58:08 +0800

    semua pengaturan di laporan.blade package pdf

--- resources/views/admin/page/laporan.blade.php
+++ resources/views/admin/page/laporan.blade.php
@@ -3,6 +3,9 @@
 @section('content')
 <div class="kt-container">
   <div class="row justify-content-center">
+    <!-- alert section -->
+      @include('admin.page.alert')
+      <!-- end alert section -->
 <div class="col-md-12">
           <div class="kt-portlet admin-portlet">
             <div class="kt-portlet__head">
@@ -13,48 +16,56 @@
 	                <h3 class="kt-portlet__head-title">
 	                  Laporan Keuangan
 	                </h3>&nbsp;
-	                <form>		
-	                			<select class="form-control-sm">
-	                				<option>pilih transaksi</option>
-				                	<option>Transaksi Alat</option>
-				                	<option>Transaksi Beras</option>
-				                	<option>Transaksi Bibit</option>
-				                	<option>Transaksi Pupuk</option>
-				                	<option>Transaksi Gabah</option>
-				                	<option>Transaksi Gadai Sawah</option>
+	                <form accept="{{route('index.laporan')}}" method="get">
+                        <select class="form-control-sm" name="jumlah">
+                          <option value="100" @if(Request::get('jumlah') == '100') {{"selected"}} @endif>100</option>
+                          <option value="50" @if(Request::get('jumlah') == '50') {{"selected"}} @endif>50</option>
+                          <option value="10" @if(Request::get('jumlah') == '10') {{"selected"}} @endif>10</option>
+                        </select>
+
+	                			<select class="form-control-sm" name="transaksi">
+	                				<option value="">pilih transaksi</option>
+				                	<option value="alat" @if(Request::get('transaksi') == 'alat') {{"selected"}} @endif >Transaksi Alat</option>
+				                	<option value="beras" @if(Request::get('transaksi') == 'beras') {{"selected"}} @endif>Transaksi Beras</option>
+				                	<option value="bibit" @if(Request::get('transaksi') == 'bibit') {{"selected"}} @endif>Transaksi Bibit</option>
+				                	<option value="pupuk" @if(Request::get('transaksi') == 'pupuk') {{"selected"}} @endif>Transaksi Pupuk</option>
+				                	<option value="gabah" @if(Request::get('transaksi') == 'gabah') {{"selected"}} @endif>Transaksi Gabah</option>
+				                	<option value="gs" @if(Request::get('transaksi') == 'gs') {{"selected"}} @endif>Transaksi Gadai Sawah</option>
 
 				                </select>
-				                <select class="form-control-sm">
-				                	<option>pilih tahun</option>
-				                	<option>2020</option>
+				                <select class="form-control-sm" name="tahun">
+				                	<option value="">pilih tahun</option>
+                          @foreach($tahun as $tahuns)
+				                	 <option value="{{$tahuns->year}}"  @if(Request::get('tahun') == $tahuns->year) {{"selected"}} @endif > {{$tahuns->year}} </option>
+                          @endforeach
 				                </select>
-				                <select class="form-control-sm">
+				                <select class="form-control-sm" name="bulan">
 				                	<option value="">pilih bulan</option>
-				                    <option value="1">Januari</option>
-				                    <option value="2">Februari</option>
-				                    <option value="3">Maret</option>
-				                    <option value="4">April</option>
-				                    <option value="5">Mei</option>
-				                    <option value="6">Juni</option>
-				                    <option value="7">Juli</option>
-				                    <option value="8">Agustus</option>
-				                    <option value="9">September</option>
-				                    <option value="10">Oktober</option>
-				                    <option value="11">November</option>
-				                    <option value="12">Desember</option>
+				                    <option value="1" @if(Request::get('bulan') == '1') {{"selected"}} @endif>Januari</option>
+				                    <option value="2" @if(Request::get('bulan') == '2') {{"selected"}} @endif>Februari</option>
+				                    <option value="3" @if(Request::get('bulan') == '3') {{"selected"}} @endif>Maret</option>
+				                    <option value="4" @if(Request::get('bulan') == '4') {{"selected"}} @endif>April</option>
+				                    <option value="5" @if(Request::get('bulan') == '5') {{"selected"}} @endif >Mei</option>
+				                    <option value="6" @if(Request::get('bulan') == '6') {{"selected"}} @endif>Juni</option>
+				                    <option value="7" @if(Request::get('bulan') == '7') {{"selected"}} @endif>Juli</option>
+				                    <option value="8" @if(Request::get('bulan') == '8') {{"selected"}} @endif>Agustus</option>
+				                    <option value="9" @if(Request::get('bulan') == '9') {{"selected"}} @endif>September</option>
+				                    <option value="10" @if(Request::get('bulan') == '10') {{"selected"}} @endif>Oktober</option>
+				                    <option value="11" @if(Request::get('bulan') == '11') {{"selected"}} @endif>November</option>
+				                    <option value="12" @if(Request::get('bulan') == '12') {{"selected"}} @endif>Desember</option>
 				                </select>
-				                <button class="btn btn-outline-primary btn-sm mr-2"> 
+				                <button type="submit" class="btn btn-outline-primary btn-sm mr-2"> 
 				                	<i class="fas fa-sync-alt"></i>Reload
 				                </button>
 				    			
-				    			<button class="btn btn-outline-danger btn-sm">
+				    			      <button class="btn btn-outline-danger btn-sm">
 				                   <i class="fas fa-file-pdf"></i> PDF
 				                </button>
 				                
 				                <button class="btn btn-outline-success btn-sm">
 				                    <i class="fas fa-file-excel"></i> Excel
 				                </button>
-				    </form>
+				        </form>
 
 	              </div>
 	              
@@ -63,50 +74,48 @@
             <div class="kt-portlet__body">
               <div class="kt-section">
                 <div class="kt-section__content">
-                  <!-- <div class="table-responsive">
-                    <table class="table table-striped">
+                  <div class="table-responsive">
+                    <table class="table table-hover">
                       <thead>
                         <tr>
                           <th>#</th>
-                          <th>Nama</th>
-                          <th>Email</th>
-                          <th>Role</th>
-                          <th>Action</th>
+                          <th>Pembeli</th>
+                          <th>Jenis Barang</th>
+                          <th>Nama Barang</th>
+                          <th>Jumlah</th>
+                          <th>Harga Satuan</th>
+                          <th>Subtotal</th>
                         </tr>
                       </thead>
                       <tbody>
+                        @php $no = 1; $alltotal = 0; @endphp
+                        @foreach($data as $datas)
                         <tr>
-                          <th scope="row">4</th>
-                          <td>tesete</td>
-                          <td>tes12</td>
-                          <td>tes</td>
-                          <td>
-                            <div class="dropdown dropdown-inline">
-                              <a href="#" class="btn btn-default btn-icon btn-icon-md btn-sm btn-more-custom" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <i class="flaticon-more-1"></i>
-                              </a>
-                              <div class="dropdown-menu dropdown-menu-right dropdown-table-custom fade" x-placement="bottom-end" style="position: absolute; will-change: transform; top: 0px; left: 0px; transform: translate3d(-149px, 33px, 0px);">
-                                <ul class="kt-nav">
-                                  <li class="kt-nav__item">
-                                    <a href="#" class="kt-nav__link edit-data" data-toggle="modal" data-target="#modal-edit-admin">
-                                      <i class="kt-nav__link-icon flaticon2-settings"></i>
-                                      <span class="kt-nav__link-text">Edit</span>
-                                    </a>
-                                  </li>
-                                  <li class="kt-nav__item">
-                                    <a href="#" style="display: none !important;" class="kt-nav__link hapus-data}">
-                                      <i class="kt-nav__link-icon flaticon2-rubbish-bin-delete-button"></i>
-                                      <span class="kt-nav__link-text">Hapus</span>
-                                    </a>
-                                  </li>
-                                </ul>
-                              </div>
-                            </div>
+                          <th scope="row">{{ $no++ }}</th>
+                          <td>{{ $datas->pembeli }}</td>
+                          <td>{{ $datas->jenis }}</td>
+                          <td>{{ $datas->nama }}</td>
+                          <td>{{ $datas->jumlah }}</td>
+                          <td>Rp. {{ format_uang($datas->harga) }}</td>
+                          <td>@if($datas->jenis == 'gadai sawah')
+                                @php $total = $datas->harga; @endphp 
+                                Rp. {{ format_uang($total) }} 
+                              @else 
+                                @php $total = $datas->harga * $datas->jumlah; @endphp 
+                                Rp. {{ format_uang($total) }}
+                              @endif
                           </td>
+                        </tr>
+                        @php $alltotal = $alltotal + $total; @endphp
+                        @endforeach
+                        <tr>
+                          <td colspan="6" align="center"><b>Total</b></td>
+                          <td><b>Rp. {{ format_uang($alltotal) }}</b></td>
                         </tr>
                       </tbody>
                     </table>
-                  </div> -->
+                      {{$data->links()}}
+                  </div>
                 </div>
               </div>
             </div>
