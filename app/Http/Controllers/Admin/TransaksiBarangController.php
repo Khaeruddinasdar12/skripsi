38e3d817bbd8e85458b536422dc41191837af273
adheee <adheejr77@gmail.com>
2020-09-17 01:18:28 +0800

    detail transaksi & riwayat transaksi, done

--- app/Http/Controllers/Admin/TransaksiBarangController.php
+++ app/Http/Controllers/Admin/TransaksiBarangController.php
@@ -8,6 +8,7 @@ use App\TransaksiBarang;
 use App\CartTransaksi;
 use App\Barang;
 use Auth;
+
 class TransaksiBarangController extends Controller
 {
     public function __construct()
@@ -17,21 +18,21 @@ class TransaksiBarangController extends Controller
 
     public function index(Request $request) //list sedang transaksi
     {
-        if($request->get('search') != '') {
+        if ($request->get('search') != '') {
             $data = TransaksiBarang::where('status', '0')
-            ->select('id', 'transaksi_code', 'penerima', 'nohp', 'alamat', 'kecamatan', 'kelurahan', 'rt', 'rw', 'total', 'keterangan', 'user_id')
-            ->with('users:id,name,nohp')
-            ->with('items:id,nama,jenis,harga,jumlah,subtotal,transaksi_id')
-            ->where('transaksi_code', 'like', '%'.$request->get('search').'%')
-            ->orderBy('created_at', 'desc')
-            ->paginate(10);
+                ->select('id', 'transaksi_code', 'penerima', 'nohp', 'alamat', 'kecamatan', 'kelurahan', 'rt', 'rw', 'total', 'keterangan', 'user_id')
+                ->with('users:id,name,nohp')
+                ->with('items:id,nama,jenis,harga,jumlah,subtotal,transaksi_id')
+                ->where('transaksi_code', 'like', '%' . $request->get('search') . '%')
+                ->orderBy('created_at', 'desc')
+                ->paginate(10);
         } else {
             $data = TransaksiBarang::where('status', '0')
-            ->select('id', 'transaksi_code', 'penerima', 'nohp', 'alamat', 'kecamatan', 'kelurahan', 'rt', 'rw', 'total', 'keterangan', 'user_id')
-            ->with('users:id,name,nohp')
-            ->with('items:id,nama,jenis,harga,jumlah,subtotal,transaksi_id')
-            ->orderBy('created_at', 'desc')
-            ->paginate(10);
+                ->select('id', 'transaksi_code', 'penerima', 'nohp', 'alamat', 'kecamatan', 'kelurahan', 'rt', 'rw', 'total', 'keterangan', 'user_id')
+                ->with('users:id,name,nohp')
+                ->with('items:id,nama,jenis,harga,jumlah,subtotal,transaksi_id')
+                ->orderBy('created_at', 'desc')
+                ->paginate(10);
         }
         $jml = TransaksiBarang::where('status', '0')
             ->count();
@@ -41,21 +42,21 @@ class TransaksiBarangController extends Controller
 
     public function riwayat(Request $request) //list sedang transaksi
     {
-        if($request->get('search') != '') {
+        if ($request->get('search') != '') {
             $data = TransaksiBarang::where('status', '1')
-            ->select('id', 'transaksi_code', 'penerima', 'nohp', 'alamat', 'kecamatan', 'kelurahan', 'rt', 'rw', 'total', 'keterangan', 'user_id')
-            ->with('users:id,name,nohp')
-            ->with('items:id,nama,jenis,harga,jumlah,subtotal,transaksi_id')
-            ->where('transaksi_code', 'like', '%'.$request->get('search').'%')
-            ->orderBy('created_at', 'desc')
-            ->paginate(10);
+                ->select('id', 'transaksi_code', 'penerima', 'nohp', 'alamat', 'kecamatan', 'kelurahan', 'rt', 'rw', 'total', 'keterangan', 'user_id')
+                ->with('users:id,name,nohp')
+                ->with('items:id,nama,jenis,harga,jumlah,subtotal,transaksi_id')
+                ->where('transaksi_code', 'like', '%' . $request->get('search') . '%')
+                ->orderBy('created_at', 'desc')
+                ->paginate(10);
         } else {
             $data = TransaksiBarang::where('status', '1')
-            ->select('id', 'transaksi_code', 'penerima', 'nohp', 'alamat', 'kecamatan', 'kelurahan', 'rt', 'rw', 'total', 'keterangan', 'user_id')
-            ->with('users:id,name,nohp')
-            ->with('items:id,nama,jenis,harga,jumlah,subtotal,transaksi_id')
-            ->orderBy('created_at', 'desc')
-            ->paginate(10);
+                ->select('id', 'transaksi_code', 'penerima', 'nohp', 'alamat', 'kecamatan', 'kelurahan', 'rt', 'rw', 'total', 'keterangan', 'user_id')
+                ->with('users:id,name,nohp')
+                ->with('items:id,nama,jenis,harga,jumlah,subtotal,transaksi_id')
+                ->orderBy('created_at', 'desc')
+                ->paginate(10);
         }
         $jml = TransaksiBarang::where('status', '1')
             ->count();
@@ -68,16 +69,16 @@ class TransaksiBarangController extends Controller
         $data = TransaksiBarang::findOrFail($id);
 
         $item = CartTransaksi::select('barang_id', 'jumlah')->where('transaksi_id', $id)->get();
-        $i=0;
+        $i = 0;
         foreach ($item as $items) {
             $dt[$i] = Barang::findOrFail($items->barang_id);
             $jmlitem[$i] = $items->jumlah;
-            if($dt[$i]['stok'] > $jmlitem) {
-                return redirect()->back()->with('error', 'Stok barang ' .$dt[$i]['nama']. ' tidak cukup. stok tersedia '.$dt[$i]['stok'].'. jumlah pesanan pelanggan '.$items->jumlah);
+            if ($dt[$i]['stok'] > $jmlitem) {
+                return redirect()->back()->with('error', 'Stok barang ' . $dt[$i]['nama'] . ' tidak cukup. stok tersedia ' . $dt[$i]['stok'] . '. jumlah pesanan pelanggan ' . $items->jumlah);
             }
             $i++;
         }
-        $a=0;
+        $a = 0;
         foreach ($dt as $update) {
             $update->stok = $update->stok - $jmlitem[$a++];
             $update->save();
@@ -96,6 +97,6 @@ class TransaksiBarangController extends Controller
         $data->keterangan = $request->get('keterangan');
         $data->status = 'batal';
         $data->save();
-        return redirect()->back()->with('success', 'Transaksi '.$data->transaksi_code.' dibatalkan');
+        return redirect()->back()->with('success', 'Transaksi ' . $data->transaksi_code . ' dibatalkan');
     }
 }
